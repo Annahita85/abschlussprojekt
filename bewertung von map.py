f1a16767d773b8b4b73d0ff4625b961e6c35f362
Annahita <73299967+Annahita85@users.noreply.github.com>
2021-06-21 10:54:42 +0200

    Add files via upload

--- bewertung von map.py
+++ bewertung von map.py
@@ -7,11 +7,11 @@ import time
 from selenium.webdriver.chrome.options import Options
 
 
-driver = webdriver.Chrome("C:/selenium beispiel/chromedriver.exe")
+driver = webdriver.Chrome("C:/Abschlussproject/abschlussprojekt/chromedriver.exe")
 
-url1 = pd.read_excel(r'Klinikliste.xlsx')[1:4]["Link Google Maps"]
+url1 = pd.read_excel(r'Klinikliste.xlsx')[1:16]["Link Google Maps"]
 
-#url = "https://www.google.de"
+
 driver.get(url1[1])
 
 driver.find_element_by_xpath('/html/body/c-wiz/div/div/div/div[2]/div[1]/div[4]/form/div[1]/div/button/span').click()
@@ -21,23 +21,13 @@ time.sleep(4)
 #scrolling
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 SCROLL_PAUSE_TIME = 0.5
+time.sleep(2)
 
 
 driver.find_element_by_css_selector('.widget-pane-link').click()
 
 content = driver.page_source
 beautisoup = BeautifulSoup(content, 'html.parser')
-""""""
-names = []
-ratings = []
-reviews = []
-departments = []
-dates = []
-titles = []
-
-
-driver.get(url)
-driver.find_element_by_xpath('/html/body/c-wiz/div/div/div/div[2]/div[1]/div[4]/form/div[1]/div/button/span').click()
 
 #scrolling
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -45,62 +35,81 @@ SCROLL_PAUSE_TIME = 0.5
 
 
 
+ratings = []
+reviews = []
+departments = []
+dates = []
+likes = []
 
-for ele in beautisoup.find_all("div", attrs={"class": "x3AX1-LfntMc-header-title-ma6Yeb-haAclf"}):
-    title = ele.find("span", attrs={"jstcache":"128"})
-    titles.append(title.text)
-    date = ele.find("time")
-    dates.append(date.text)
-    department = ele.find("a", href=True)
-    departments.append(department.text)
-    review = ele.find("p")
-    reviews.append(review.text)
-
-    rating = ele.find("dd")
-    ratings.append(rating.text)
-
-x = len(titles)
-
-# for ele in beautisoup.find_all("section", attrs={"class":"rating"}):
+rating = driver.find_elements_by_xpath('//div[@class="ODSEW-ShBeI-jfdpUb"]/span[2]')
+review = driver.find_element_by_xpath('//[@div/div/div[1]/div/div/div[32]/div/div[3]/div[2]/div/div/a/div[2]/span[2]')
+department = driver.find_element_by_xpath('//div[@class="ODSEW-ShBeI-ShBeI-content"]/span[2]')
+date = driver.find_element_by_xpath('//span[@class="ODSEW-ShBeI-RgZmSc-date"]')
+like = driver.find_element_by_xpath('//button[@class="ODSEW-ShBeI-Sc2xXc-LgbsSe"]/span/span[2]')
 
 
-for ele in beautisoup.find_all("div", attrs={"class": "klinik-normal"}):
-    name = ele.find("h1")
-    names.append(name.text)
-names = names * x
-#ratings = ratings * x
-df = pd.DataFrame({'Klinik_Name': names, 'Title': titles, 'Date': dates,
-                   'Department': departments, 'rating': ratings, 'Erfahrungsbericht & Review': reviews})
-df.to_csv('D:/refugeeks_project/selenium_py/p1_py/klinik1_liste1.csv',
-          index=False, encoding='utf-8')
+for txt in rating:
+    ratings = txt.get_attribute("aria-label")[1]
 
+for txt in review:
+    reviews = txt.text
 
+for txt in department:
+    departments = txt.text
 
+for txt in date:
+    dates = txt.text        
 
+for txt in like:
+    likes = txt.text
+
+df = pd.DataFrame(zip(ratings,reviews,departments,dates,likes), columns=["Rating","Reviw","Department","Date",'Likes'])
+df.to_csv('GoogleMaps.csv', index=False)    
 
 
 
 
 
+for ele in driver.find_all("div", attrs={"class": "x3AX1-LfntMc-header-title-ma6Yeb-haAclf"}):
+     # title = ele.find("span", attrs={"jstcache":"128"})
+     # titles.append(title.text)
+     date = driver.findElement(By.xpath("//*[@id="pane"]/div/div[1]/div/div/div[32]/div/div[3]/div[3]/div[1]/span[3]"))
+     dates.append(date.text)
+     department = ele.find.element #(//*[@id="pane"]/div/div[1]/div/div/div[32]/div/div[3]/div[3]/div[1]/span[2])
+     departments.append(department.text) 
+     review = ele.find("span", attrs={"jstcache":"509"})
+     reviews.append(review.text)
+
+     rating = ele.find("span", attrs={"jstcache":"408"})
+     ratings.append(rating.text)
+
+     like = ele.find("span", attrs={"jstcache":"507"})
+     likes.append(like.text)
+
+
+ for ele in beautisoup.find_all("section", attrs={"class":"rating"}):
+
+
+ for ele in beautisoup.find_all("div", attrs={"class": "klinik-normal"}):
+     name = ele.find("h1")
+     names.append(name.text)
+ names = names * x
+ ratings = ratings * x
+ df = pd.DataFrame({'Klinik_Name': names, 'Title': titles, 'Date': dates,
+                    'Department': departments, 'rating': ratings, 'Erfahrungsbericht & Review': reviews})
+ df.to_csv('D:/refugeeks_project/selenium_py/p1_py/klinik1_liste1.csv',
+           index=False, encoding='utf-8')
 
 
 
 
 
-# Get scroll height
-last_height = driver.execute_script("return document.body.scrollHeight")
 
-while True:
-    # Scroll down to bottom
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    # Wait to load page
-    time.sleep(SCROLL_PAUSE_TIME)
 
-    # Calculate new scroll height and compare with last scroll height
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
 
-    label.sendKeys(Keys.PAGE_DOWN);
+
+
+
+
+
